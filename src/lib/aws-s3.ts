c47aad034d169373ff9dcbffcdc4a04ec50a4ace
liacodify <liacodify@gmail.com>
2026-01-28 10:28:57 -0500

    arreglo de aws

--- src/lib/aws-s3.ts
+++ src/lib/aws-s3.ts
@@ -1,13 +1,13 @@
 import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
 
 export const s3 = new S3Client({
-  region: process.env.S3_REGION || "us-east-1", // tu región
+  endpoint: process.env.S3_ENDPOINT, // 🔴 CLAVE
+  region: process.env.S3_REGION || "us-east-1",
   credentials: {
     accessKeyId: process.env.MYAPP_AWS_ACCESS_KEY_ID!,
     secretAccessKey: process.env.MYAPP_AWS_SECRET_ACCESS_KEY!,
   },
-
-  forcePathStyle: true,
+  forcePathStyle: true, // obligatorio para MinIO
 });
 
 export async function getFileContent(key: string): Promise<string> {
